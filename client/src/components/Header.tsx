0da3d8dc703a979a26b6f8e982299211a1926f68
“Chrispin <c43057772@gmail.com>
2025-11-13 16:37:23 +0300

    Added proper naming title for the website

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -181,7 +181,7 @@ const Header = () => {
               </div>
               <div className="text-white">
                 <div className="text-xs font-medium opacity-90">Talk to us</div>
-                <div className="text-sm font-semibold">+254 729407573</div>
+                <div className="text-sm font-semibold">+254710899700</div>
               </div>
             </div>
 
